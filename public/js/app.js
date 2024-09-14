c094825aedbbb704c64199e1acfeb72d48cafbcb
kennethleanda <leandakenneth@gmail.com>
2024-09-15 01:38:39 +0800

    UX  for increasing and decrease of Item quantity inside the cart

--- public/js/app.js
+++ public/js/app.js
@@ -25,20 +25,29 @@ function displayCartItems() {
      // Check if there are any items in the cart
     if (cartItems.length > 0) {
         let displayString = '';
+        let grandTotal = 0;
         cartItems.forEach(item => {
 
             const totalPrice = (item.price * item.quantity).toFixed(2);
-
+            grandTotal += parseFloat(totalPrice);
             displayString += `
-                    <a href="/cart" class="fw-bold text-black text-uppercase">${item.name}</a>
+                    <div class='d-flex align-items-center gap-2'>
+                        <a href="/cart" class="fw-bold text-black text-uppercase">${item.name}</a> 
+                        <a href="" onclick="removeFromCart(event, ${item.id})" class=" text-danger"><i class='bx bx-trash' ></i></a> 
+                    </div>
+                    
                     <p class="text-danger">$${item.price}</p>
-                    <p>Qty: ${item.quantity} </p>
+                    <div class='d-flex gap-2'>
+                        <p class=''>Qty: ${item.quantity} </p>
+                        <a href="" onclick="addQty(event, ${item.id})" class=" text-black"><i class='bx bx-plus' ></i></a>
+                        <a href="" onclick="minusQty(event, ${item.id})" class=" text-black"><i class='bx bx-minus' ></i></a> 
+                    </div>
                     <p class="fw-bold">Total: $${totalPrice}</p>
-                    <a href="" onclick="removeFromCart(event, ${item.id})" class=" text-danger"><i class='bx bx-trash' ></i></a>
+                    
                     <hr>
                 `;
         });
-
+        displayString += `<p class="fw-bold text-success">Grand Total: $${grandTotal}</p>`;
          //To be displayed on dropdown of cart icon 
         const cartIcon = document.getElementById('cart-icon-items');
         if (cartIcon) cartIcon.innerHTML = displayString;
@@ -57,6 +66,47 @@ function displayCartItems() {
         if (cartIcon) cartIcon.innerHTML = '<p>No items in the cart.</p>';
         
     }
+}
+
+function addQty(event, id){
+    event.preventDefault();
+
+    let cartItems = JSON.parse(sessionStorage.getItem('cartItems')) || [];
+
+    const itemIndex = cartItems.findIndex(item => item.id === id);
+
+    //The same concept when adding an item to cart.
+    // It will check if it is existing on the cart.
+    // In our case, we know that it already exists on our cart, so we are just going to increase its quantity.
+
+    if (itemIndex > -1) {
+        cartItems[itemIndex].quantity += 1;
+    } 
+
+    sessionStorage.setItem('cartItems', JSON.stringify(cartItems));
+    displayCartItems();
+}
+
+function minusQty(event, id){
+    event.preventDefault();
+
+    let cartItems = JSON.parse(sessionStorage.getItem('cartItems')) || [];
+
+    const itemIndex = cartItems.findIndex(item => item.id === id);
+
+    if (itemIndex > -1) {
+        cartItems[itemIndex].quantity -= 1;
+
+        //Remove the item if its quantity reaches 0
+        // splice is an array method that removes an element inside an array. 
+        // In our case, at index (itemIndex), we are going to remove 1 item
+        if(cartItems[itemIndex].quantity <= 0){
+            cartItems.splice(itemIndex, 1)
+        }
+    } 
+
+    sessionStorage.setItem('cartItems', JSON.stringify(cartItems));
+    displayCartItems();
 }
 
 function addToCart(id, name, price) {
